298e5615b724585075955e613ed412bd5e8b9d06
s90246 <s90246@bht-berlin.de>
2025-07-12 14:33:31 +0200

    impl of product editing

--- customerService/urls.py
+++ customerService/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('add-artist/', views.addArtist, name='add_artist'),
     path('add-album/', views.addAlbum, name='add_album'),
     path('add-track/', views.addTrack, name='add_track'),
-    path('modify-artist/', views.modify_artist, name='modify_artist'),
+    path('modify-artist/', views.ModifyArtistView.as_view(), name='modify_artist'),
+    path('modify-album/', views.ModifyAlbumView.as_view(), name='modify_album'),
+    path('modify-track/', views.ModifyTrackView.as_view(), name='modify_track'),
+    path("reviews/", views.showReviews, name="reviews")
     
 ]
--- customerService/views.py
+++ customerService/views.py
@@ -3,6 +3,7 @@ from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from store.models import Track, Album, Artist
+from django.views import View
 from django.shortcuts import redirect
 from datetime import timedelta
 
@@ -19,6 +20,75 @@ class AddProductView(TemplateView):
 
 class ModifyProductView(TemplateView):
     template_name = "customerService/modifyProduct.html"
+
+class ModifyArtistView(View):
+    def get(self, request):
+        return render(request, "customerService/modifyArtist.html")
+
+    def post(self, request):
+        name = request.POST.get("name")
+        genre = request.POST.get("genre")
+        dateOfBirth = request.POST.get("dateOfBirth")
+        country = request.POST.get("country")
+        bio = request.POST.get("bio")
+
+        artist = Artist.objects.first()  
+        artist.name = name
+        artist.genre = genre
+        artist.dateOfBirth = dateOfBirth
+        artist.country = country
+        artist.bio = bio
+        artist.save()
+
+        return redirect("customerService")
+    
+
+class ModifyAlbumView(View):
+    def get(self, request):
+        return render(request, "customerService/modifyAlbum.html")
+    
+    def post(self, request):
+        title = request.POST.get("title")
+        artist_name = request.POST.get("artist")
+        releaseDate = request.POST.get("releaseDate")
+        price = request.POST.get("price")
+
+        album = Album.objects.first()
+        artist = Artist.objects.get(name=artist_name)
+
+        album.title = title
+        album.artist = artist
+        album.releaseDate = releaseDate
+        album.price = price
+        album.save()
+
+        return redirect("customerService")
+        
+
+
+
+class ModifyTrackView(View):
+    def get(self, request):
+        return render(request, "customerService/modifyTrack.html")
+    
+    def post(self, request):
+        title = request.POST.get("title")
+        album_name = request.POST.get("album")
+        duration_str = request.POST.get("duration")
+
+        track = Track.objects.first()
+
+        album = Album.objects.get(title=album_name)
+
+        h, m, s = map(int, duration_str.split(':'))
+        duration = timedelta(hours=h, minutes=m, seconds=s)
+
+        track.title = title
+        track.album = album
+        track.duration = duration
+        track.save()
+
+        return redirect("customerService")
 
 
 def addArtist(request):
@@ -76,5 +146,7 @@ def addTrack(request):
     return render(request, "csPortal.html")
 
 
-def modify_artist(self):
-    print("do")
+def showReviews(request):
+    if request.method == "GET":
+        return render(request, "customerService/editReviews.html")
+    return render(request, "csPortal.html")
